754f3410c9a48b35a3b9d3f03d1679cb9c159913
Hector Partidas <hpartidas@deuz.net>
2017-10-01 13:42:35 -0500

    Update portfolio deployment function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,23 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('hpartidas.guru')
-build_bucket = s3.Bucket('build.hpartidas.guru')
+    portfolio_bucket = s3.Bucket('hpartidas.guru')
+    build_bucket = s3.Bucket('build.hpartidas.guru')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('build.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('build.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(
-            obj,
-            nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]}
-        )
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(
+                obj,
+                nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]}
+            )
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Deployment complete!"
