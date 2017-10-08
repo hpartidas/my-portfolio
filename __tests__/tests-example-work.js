334f741fdc264300e1faeb2bd3187757c2f953d0
Hector Partidas <hpartidas@deuz.net>
2017-10-08 18:39:12 -0500

    Added unit tests with Jest and Enzyme

---
+++ __tests__/tests-example-work.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ExampleWork, {ExampleWorkBubble} from '../js/components/example-work';
+
+global.requestAnimationFrme = function(callback) {
+    setTimeout(callback, 0);
+};
+
+configure({
+  adapter: new Adapter()
+});
+
+const myWork = [
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+           https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  }
+];
+
+describe("ExampleWork Component", () => {
+
+  let component = shallow(<ExampleWork work={myWork} />);
+
+  it("Should be a 'section' element", () => {
+    expect(component.type()).toEqual('section');
+  });
+
+  it("Should contain as many children as there are work examples", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
+});
+
+describe("ExampleWorkBubble component", () =>{
+  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+
+  let images = component.find("img");
+
+  it("Should contain a single image element", () => {
+    expect(images.length).toEqual(1);
+  });
+
+  it("Should have the image source set correctly", () => {);
+    expect(images.getElement().props.src).toEqual(myWork[1].image.src);
+  });
+});
