09938e0734305ffc822b28bcdcd3134b20c76c49
Hector Partidas <hector.partidas@admios-sa.com>
2017-10-16 10:59:17 -0500

    Spacing update, down to 2 spaces from 4. Updated favicon and added support for all browsers.

--- __tests__/tests-example-work-modal.js
+++ __tests__/tests-example-work-modal.js
@@ -1,52 +1,52 @@
 import React from 'react';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-import { shallow, configure } from 'enzyme';
+import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ExampleWorkModal from '../js/components/example-work-modal';
 
-global.requestAnimationFrme = function(callback) {
-    setTimeout(callback, 0);
+global.requestAnimationFrme = function (callback) {
+  setTimeout(callback, 0);
 };
 
 configure({
-    adapter: new Adapter()
+  adapter: new Adapter()
 });
 
 const myExample = {
-        'title': "Work Example",
-        'href': "https://example.com",
-        'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur corporis cum cupiditate deserunt dignissimos eius ex harum ipsum, labore maiores, minima nesciunt officia perferendis quasi qui quidem sunt voluptatibus!",
-        'image': {
-            'desc': "example screenshot of a project involving code",
-            'src': "images/example1.png",
-            'comment': ""
-        }
-    };
+  'title': "Work Example",
+  'href': "https://example.com",
+  'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur corporis cum cupiditate deserunt dignissimos eius ex harum ipsum, labore maiores, minima nesciunt officia perferendis quasi qui quidem sunt voluptatibus!",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+  }
+};
 
 describe("ExampleWorkModal component", () => {
-    let mockCloseModalFn = jest.fn();
+  let mockCloseModalFn = jest.fn();
 
-    let component = shallow(<ExampleWorkModal example={myExample} open={false} closeModal={mockCloseModalFn}/>);
-    let openComponent = shallow(<ExampleWorkModal example={myExample} open={true}/>);
+  let component = shallow(<ExampleWorkModal example={myExample} open={false} closeModal={mockCloseModalFn}/>);
+  let openComponent = shallow(<ExampleWorkModal example={myExample} open={true}/>);
 
-    let anchors =  component.find("a");
+  let anchors = component.find("a");
 
-    it("Should contain a single 'a' element", () => {
-        expect(anchors.length).toEqual(1);
-    });
+  it("Should contain a single 'a' element", () => {
+    expect(anchors.length).toEqual(1);
+  });
 
-    it("Should link to our project", () => {
-        expect(anchors.getElement().props.href).toEqual(myExample.href);
-    });
+  it("Should link to our project", () => {
+    expect(anchors.getElement().props.href).toEqual(myExample.href);
+  });
 
-    it("Should have the modal class set correctly", () => {
-        expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
-        expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
-    });
+  it("Should have the modal class set correctly", () => {
+    expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
+    expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
+  });
 
-    it("Should call the closeModal handler when clicked", () => {
-        component.find(".modal__closeButton").simulate('click');
-        expect(mockCloseModalFn).toHaveBeenCalled();
-    });
+  it("Should call the closeModal handler when clicked", () => {
+    component.find(".modal__closeButton").simulate('click');
+    expect(mockCloseModalFn).toHaveBeenCalled();
+  });
 });
--- __tests__/tests-example-work.js
+++ __tests__/tests-example-work.js
@@ -7,71 +7,71 @@ import ExampleWork, {ExampleWorkBubble} from '../js/components/example-work';
 import ExampleWorkModal from "../js/components/example-work-modal";
 
 global.requestAnimationFrme = function (callback) {
-    setTimeout(callback, 0);
+  setTimeout(callback, 0);
 };
 
 configure({
-    adapter: new Adapter()
+  adapter: new Adapter()
 });
 
 const myWork = [
-    {
-        'title': "Work Example",
-        'image': {
-            'desc': "example screenshot of a project involving code",
-            'src': "images/example1.png",
-            'comment': ""
-        }
-    },
-    {
-        'title': "Work Example",
-        'image': {
-            'desc': "example screenshot of a project involving chemistry",
-            'src': "images/example2.png",
-            'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
-           https://www.flickr.com/photos/ssoosay/4097410999`
-        }
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
     }
+  },
+  {
+    'title': "Work Example",
+    'image': {
+      'desc': "example screenshot of a project involving chemistry",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+           https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  }
 ];
 
 describe("ExampleWork Component", () => {
 
-    let component = shallow(<ExampleWork work={myWork}/>);
+  let component = shallow(<ExampleWork work={myWork}/>);
 
-    it("Should be a 'span' element", () => {
-        expect(component.type()).toEqual('span');
-    });
+  it("Should be a 'span' element", () => {
+    expect(component.type()).toEqual('span');
+  });
 
-    it("Should contain as many children as there are work examples", () => {
-        expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
-    });
+  it("Should contain as many children as there are work examples", () => {
+    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+  });
 
-    it("Should allow the modal to open and close", () => {
-        component.instance().openModal();
-        expect(component.instance().state.modalOpen).toBe(true);
-        component.instance().closeModal();
-        expect(component.instance().state.modalOpen).toBe(false);
+  it("Should allow the modal to open and close", () => {
+    component.instance().openModal();
+    expect(component.instance().state.modalOpen).toBe(true);
+    component.instance().closeModal();
+    expect(component.instance().state.modalOpen).toBe(false);
 
-    });
+  });
 });
 
 describe("ExampleWorkBubble component", () => {
-    let mockOpenModalFn = jest.fn();
+  let mockOpenModalFn = jest.fn();
 
-    let component = shallow(<ExampleWorkBubble example={myWork[1]} openModal={mockOpenModalFn}/>);
+  let component = shallow(<ExampleWorkBubble example={myWork[1]} openModal={mockOpenModalFn}/>);
 
-    let images = component.find("img");
+  let images = component.find("img");
 
-    it("Should contain a single image element", () => {
-        expect(images.length).toEqual(1);
-    });
+  it("Should contain a single image element", () => {
+    expect(images.length).toEqual(1);
+  });
 
-    it("Should have the image source set correctly", () => {
-        expect(images.getElement().props.src).toEqual(myWork[1].image.src);
-    });
+  it("Should have the image source set correctly", () => {
+    expect(images.getElement().props.src).toEqual(myWork[1].image.src);
+  });
 
-    it("Should call the openModal handler when clicked", () => {
-       component.find(".section__exampleWrapper").simulate('click');
-       expect(mockOpenModalFn).toHaveBeenCalled();
-    });
+  it("Should call the openModal handler when clicked", () => {
+    component.find(".section__exampleWrapper").simulate('click');
+    expect(mockOpenModalFn).toHaveBeenCalled();
+  });
 });
