ee8403f8a7919e6ea7b08a2e14582338c6a8829b
Hector Partidas <hpartidas@deuz.net>
2017-10-16 00:55:26 -0500

    Added ExampleWorkModal compnent Added State management to ExampleWork component

--- __tests__/tests-example-work.js
+++ __tests__/tests-example-work.js
@@ -1,61 +1,77 @@
 import React from 'react';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
-import { shallow, configure } from 'enzyme';
+import {shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ExampleWork, {ExampleWorkBubble} from '../js/components/example-work';
+import ExampleWorkModal from "../js/components/example-work-modal";
 
-global.requestAnimationFrme = function(callback) {
+global.requestAnimationFrme = function (callback) {
     setTimeout(callback, 0);
 };
 
 configure({
-  adapter: new Adapter()
+    adapter: new Adapter()
 });
 
 const myWork = [
-  {
-    'title': "Work Example",
-    'image': {
-      'desc': "example screenshot of a project involving code",
-      'src': "images/example1.png",
-      'comment': ""
-    }
-  },
-  {
-    'title': "Work Example",
-    'image': {
-      'desc': "example screenshot of a project involving chemistry",
-      'src': "images/example2.png",
-      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a project involving chemistry",
+            'src': "images/example2.png",
+            'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
            https://www.flickr.com/photos/ssoosay/4097410999`
+        }
     }
-  }
 ];
 
 describe("ExampleWork Component", () => {
 
-  let component = shallow(<ExampleWork work={myWork} />);
+    let component = shallow(<ExampleWork work={myWork}/>);
 
-  it("Should be a 'section' element", () => {
-    expect(component.type()).toEqual('section');
-  });
+    it("Should be a 'span' element", () => {
+        expect(component.type()).toEqual('span');
+    });
 
-  it("Should contain as many children as there are work examples", () => {
-    expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
-  });
+    it("Should contain as many children as there are work examples", () => {
+        expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+    });
+
+    it("Should allow the modal to open and close", () => {
+        component.instance().openModal();
+        expect(component.instance().state.modalOpen).toBe(true);
+        component.instance().closeModal();
+        expect(component.instance().state.modalOpen).toBe(false);
+
+    });
 });
 
-describe("ExampleWorkBubble component", () =>{
-  let component = shallow(<ExampleWorkBubble example={myWork[1]}/>);
+describe("ExampleWorkBubble component", () => {
+    let mockOpenModalFn = jest.fn();
 
-  let images = component.find("img");
+    let component = shallow(<ExampleWorkBubble example={myWork[1]} openModal={mockOpenModalFn}/>);
 
-  it("Should contain a single image element", () => {
-    expect(images.length).toEqual(1);
-  });
+    let images = component.find("img");
 
-  it("Should have the image source set correctly", () => {);
-    expect(images.getElement().props.src).toEqual(myWork[1].image.src);
-  });
+    it("Should contain a single image element", () => {
+        expect(images.length).toEqual(1);
+    });
+
+    it("Should have the image source set correctly", () => {
+        expect(images.getElement().props.src).toEqual(myWork[1].image.src);
+    });
+
+    it("Should call the openModal handler when clicked", () => {
+       component.find(".section__exampleWrapper").simulate('click');
+       expect(mockOpenModalFn).toHaveBeenCalled();
+    });
 });
--- js/main.js
+++ js/main.js
@@ -3,32 +3,38 @@ import ReactDOM from 'react-dom';
 import ExampleWork from './components/example-work';
 
 const myWork = [
-  {
-    'title': "Work Example",
-    'image': {
-      'desc': "example screenshot of a project involving code",
-      'src': "images/example1.png",
-      'comment': ""
-    }
-  },
-  {
-    'title': "Work Example",
-    'image': {
-      'desc': "example screenshot of a project involving chemistry",
-      'src': "images/example2.png",
-      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur corporis cum cupiditate deserunt dignissimos eius ex harum ipsum, labore maiores, minima nesciunt officia perferendis quasi qui quidem sunt voluptatibus!",
+        'image': {
+            'desc': "example screenshot of a project involving code",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur corporis cum cupiditate deserunt dignissimos eius ex harum ipsum, labore maiores, minima nesciunt officia perferendis quasi qui quidem sunt voluptatibus!",
+        'image': {
+            'desc': "example screenshot of a project involving chemistry",
+            'src': "images/example2.png",
+            'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
            https://www.flickr.com/photos/ssoosay/4097410999`
-    }
-  },
-  {
-    'title': "Work Example",
-    'image': {
-      'desc': "example screenshot of a project involving cats",
-      'src': "images/example3.png",
-      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+        }
+    },
+    {
+        'title': "Work Example",
+        'href': "https://example.com",
+        'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur corporis cum cupiditate deserunt dignissimos eius ex harum ipsum, labore maiores, minima nesciunt officia perferendis quasi qui quidem sunt voluptatibus!",
+        'image': {
+            'desc': "example screenshot of a project involving cats",
+            'src': "images/example3.png",
+            'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
            https://www.flickr.com/photos/37287295@N00/2540855181`
+        }
     }
-  }
-]
+];
 
-ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
